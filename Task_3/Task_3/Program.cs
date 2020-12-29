9758e19e59f7d7e5251dba587f5e6d0cbbe0a257
Павел Мельников <melnikovgru@gmail.com>
2020-12-30 01:15:55 +0300

    add all object in array and write them

--- Task_3/Task_3/Program.cs
+++ Task_3/Task_3/Program.cs
@@ -2,26 +2,30 @@
 
 namespace Task_3
 {
-    class Purchase: IComparable 
+    public enum Days
     {
-        private string NameItem { get; set; }
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    class Purchase : IComparable<Purchase>
+    {
+        public Days days = new Days();
+        public string NameItem { get; set; }
+
+        //private const string nameItem = NameItem;
         public double Cost { get; set; }
         public int Amount { get; set; }
-        public enum Days
-        {
-            Monday,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday,
-            Sunday
-        }
+        
 
         // constructor
-        public Purchase(){}
+        public Purchase() { }
 
-        public Purchase(string N, double C, int A) { NameItem = N; Cost = C; Amount = A;}
+        public Purchase(string N, double C, int A, Days D) { NameItem = N; Cost = C; Amount = A; days = D;}
 
         public double GetCost()
         {
@@ -29,19 +33,15 @@ namespace Task_3
         }
         public override string ToString()
         {
-            return ($"Name of product: {NameItem} Cost of product: {Cost}  Amount total: {Amount} summ total: {GetCost()}");
+            return $"Name of product: {NameItem} Cost of product: {Cost}  Amount total: {Amount} day : {days} summ total: {GetCost()}";
         }
 
-        public int CompareTo(object obj)
+        public int CompareTo(Purchase I)
         {
-            Purchase p = obj as Purchase;
-
+            Purchase p = I;
             Console.WriteLine(p.Cost);
             Console.WriteLine(Cost);
-            if (p != null)
                 return Cost.CompareTo(p.Cost);
-            else
-            throw new Exception("unpossible compare both objects");
         }
     }
 
@@ -49,10 +49,20 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
-            Purchase ObjOne = new Purchase("netbook", 300.44, 3);
-            Purchase ObjTwo = new Purchase("iphone", 500.65, 2);
-            
-            Console.WriteLine(ObjOne.CompareTo(ObjTwo));
+
+            Purchase[] purchases = new Purchase[] {
+                  new Purchase("netbook", 300.44, 3, Days.Monday),
+                  new Purchase("iphone", 500.65, 2, Days.Thursday),
+                  new Purchase("macbook", 800.85, 4, Days.Tuesday),
+                  new Purchase("PS", 750, 8, Days.Wednesday),
+                  new Purchase("X-Box", 740.90, 6, Days.Sunday),
+            };
+
+            foreach(object O in purchases)
+            {
+                Console.WriteLine(O);
+            }
+           //Arrays.sort(purchases)
         }
     }
 }
