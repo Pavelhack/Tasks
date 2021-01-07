3c2da36f10aead3572c36987fa1e73201a121b17
Павел Мельников <melnikovgru@gmail.com>
2021-01-08 01:13:15 +0300

    fix to Discont class / it work

--- Task_4/Task_4/Program.cs
+++ Task_4/Task_4/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Task_4
 {
-    public class Market : IComparable<Market>
+    public class Market 
     {
         public string Title { get; set; }
 
@@ -10,54 +10,52 @@ namespace Task_4
 
         public int Amount { get; set; }
 
-        public double GetCost()
+        public virtual double GetCost()
         {
             return Amount * Cost;
         }
 
         public override string ToString()
         {
-           return $"Name of item: {Title}; Cost of item: {Cost}; Amount of items{Amount}; Total summ: {GetCost()}"; 
+           return $"Name of item: {Title}; Cost of item: {Cost}; Amount of items: {Amount}; Total summ: {GetCost()}"; 
         }
 
-        public int Equals(Market X, Market Y)
-        {
-            return X.CompareTo(Y);
-        }
-        public int CompareTo(Market I)
-        {
-            Market p = I;
-            int first =  Cost.CompareTo(p.Cost);
-            int second =  Title.CompareTo(p.Title);
-             if (first == second) {
-                return 1;
-             } else{
-                return 0;
-            } 
-        }
+        public Market() { }
 
-        
+        public Market(string S, double D, int I) { Title = S; Cost = D; Amount = I; }
+
     }
+
     public class Discont : Market 
     {
-        
-    }
-    public class Goods : Market
-    {
+        public override double GetCost()
+        {
+          return (Amount * Cost) * 85 / 100;
+        }
+        public Discont(string S, double D, int I) { Title = S; Cost = D; Amount = I; }
 
     }
+    //public class Goods
+    //{
+    //    public int 
+    //}
     class Program
     {
         static void Main(string[] args)
-        {   
+        {
             Market[] purchases = new Market[] {
-                  new Market{Title = "X-Box", Cost =  740.90, Amount = 6},
-                  new Market{Title = "X-Box2", Cost =  980.30, Amount =  4},
-                  new Discont{Title = "netbook",Cost =  300.44, Amount = 5 },
-                  new Discont{Title = "iphone",Cost =  500.65, Amount =  2 },
-                  new Goods{Title = "macbook",Cost =  800.85, Amount =  4 },
-                  new Goods{Title = "PS",Cost =  750, Amount =  8 },
+                  new Market("X-Box", 740.90, 6),
+                  new Market("X-Box2", 980.30, 6),
+                  new Discont("netbook", 300.44, 3),
+                  new Discont("iphone", 500.65, 2),
+                  //new Goods("macbook", 800.85, 4),
+                  //new Goods("PS", 750, 8),
             };
+
+            foreach(Market O in purchases)
+            {
+                Console.WriteLine(O);
+            }
 
         }
     }
